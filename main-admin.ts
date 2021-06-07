7bb33e958842c204f5b8aa9996ec66a7bbfa91d4
michaschwab <gjplickscoconutthunderstormb>
2021-06-07 17:33:18 -0400

    admin panel now shows a table of the videos of the track

--- main-admin.ts
+++ main-admin.ts
@@ -16,6 +16,31 @@ class IeeeVisStreamAdmin {
         this.data = track;
 
         document.getElementById('track-title').innerText = this.data.name;
+
+        this.updateTable();
+
+        setInterval(this.updateTable.bind(this), 1000);
+    }
+
+    updateTable() {
+        const tableBody = document.getElementById('videos-table-body') as HTMLTableElement;
+        tableBody.innerHTML = '';
+
+        console.log(this.data.videos);
+        const currentVideoPlayedMs = new Date().getTime() - this.data.currentStatus.videoStartTimestamp;
+
+        for(const videoKey in this.data.videos) {
+            const video = this.data.videos[videoKey];
+            const active = this.data.currentStatus.videoIndex.toString() === videoKey;
+
+            const timePlayed = !active ? '-' : new Date(currentVideoPlayedMs).toISOString().substr(11, 8);
+
+            const tr = document.createElement('tr');
+            tr.className = active ? 'active' : '';
+            tr.innerHTML = `<td>${video.title}</td><td>${video.type}</td><td>${timePlayed}</td>`;
+
+            tableBody.append(tr);
+        }
     }
 }
 
